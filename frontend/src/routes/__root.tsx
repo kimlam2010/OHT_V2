c664d4a94083768a8a913cdfaf487682f5d3395b
NguyênNguyên <dev.nguyennguyen@gmail.com>
2025-09-04 14:36:29 +0700

    feat: setup font family and palette color project

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import Header from '@/components/layouts/Header'
 import { Plugins } from '@/plugins'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
       <Plugins>
         <Outlet />
       </Plugins>
