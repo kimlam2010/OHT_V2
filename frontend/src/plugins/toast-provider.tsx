582417804e4c5479bc2b728b9f8dfb384a32af65
NguyênNguyên <dev.nguyennguyen@gmail.com>
2025-09-25 10:49:51 +0700

    chore: update toast-provider plugin

--- frontend/src/plugins/toast-provider.tsx
+++ frontend/src/plugins/toast-provider.tsx
@@ -1,6 +1,18 @@
-import { Toaster } from 'react-hot-toast'
+import { useEffect } from 'react'
+import toast, { Toaster, useToasterStore } from 'react-hot-toast'
+
+const TOAST_LIMIT = 3
 
 export function ToastProvider() {
+  const { toasts } = useToasterStore()
+
+  // Enforce Limit
+  useEffect(() => {
+    toasts
+      .filter(t => t.visible) // Only consider visible toasts
+      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit
+      .forEach(t => toast.dismiss(t.id)) // Dismiss – Use toast.remove(t.id) removal without animation
+  }, [toasts])
   return (
     <Toaster
       position="top-center"
